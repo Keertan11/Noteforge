5b6c3a70e9a541127c9c8c54adddfd7987e61892
Keertan <keertanvashisht2005@gmail.com>
2026-02-28 19:49:01 +0530

    feat: add notebook and note management APIs, including CRUD operations for notebooks and notes, and user authentication for sign-in and sign-up

---
+++ components/notebooks.tsx
@@ -0,0 +1,10 @@
+export default function Notebooks() {
+    return (
+        <div className="mt-4">
+            <h2 className="text-xl font-semibold">Your Notebooks</h2>
+
+            {/* Placeholder for notebooks list */}
+            <p className="text-gray-500 mt-2">You have no notebooks yet. Create one to get started!</p>
+        </div>
+    );
+}
---
+++ lib/auth.ts
@@ -0,0 +1,45 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../db/drizzle";
+import { schema } from "../db/schema"
+import { nextCookies } from "better-auth/next-js";
+import { Resend } from "resend";
+import VerificationEmail from "@/components/emails/verification-email";
+import PasswordResetEmail from "@/components/emails/reset-email";
+
+const resend = new Resend(process.env.RESEND_API_KEY!);
+export const auth = betterAuth({
+    emailVerification: {
+        sendVerificationEmail: async ({ user, url }) => {
+            await resend.emails.send({
+                from: 'NoteForge <noteforge@orcdev.com>',
+                to: [user.email],
+                subject: 'Verify your email address',
+                react: VerificationEmail({ userName: user.name, verificationUrl: url }),
+            });
+        },
+        sendOnSignUp: true,
+    },
+    emailAndPassword: {
+        enabled: true,
+        sendResetPassword: async ({ user, url }) => {
+            await resend.emails.send({
+                from: 'Resend <onboarding@resend.dev>',
+                to: [user.email],
+                subject: 'Reset your password',
+                react: PasswordResetEmail({ userName: user.name, resetUrl: url, requestTime: new Date().toLocaleString() }),
+            });
+        },
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema,
+    }),
+    plugins: [nextCookies()]
+});
